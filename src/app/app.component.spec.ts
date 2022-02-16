5259bb3e54ffa1484ab1f97f42eec2b227037821
Glacy <glacy.kelly3@gmail.com>
2022-02-16 20:04:28 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -19,7 +19,6 @@ describe('AppComponent', () => {
   it(`should have as title 'cart-shopping'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('cart-shopping');
   });
 
   it('should render title', () => {
